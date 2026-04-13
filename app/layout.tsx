abd95ea387ed68008d79faedf19a2a2358d03335
Rohith P M <rohithpm@Rohiths-MacBook-Air.local>
2026-04-13 21:46:22 +0530

    Remove mistakenly added Google Tag Manager snippet

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Script from "next/script";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,23 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script id="gtm-head" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-NCLNLNGN');`}
-        </Script>
       </head>
       <body className={inter.className} suppressHydrationWarning>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NCLNLNGN"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
+
         {children}
       </body>
     </html>
